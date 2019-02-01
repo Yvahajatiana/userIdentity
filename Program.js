cee082615db262ecb1202f9c91e5c0aee3cf6740
Yva Hajatiana <yvahajatiana@gmail.com>
2019-02-01 12:25:15 +0400

    add dotenv and make port to dynamic

--- Program.js
+++ Program.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const http = require('http');
 const startup = require('./startup');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+console.log("server run with port: "+port);
 const server = http.createServer(startup);
 
 server.listen(port);
